1777d976defb61936aa844b595d7457e490be431
Salimul Hashir <162561129+hash-4oh4@users.noreply.github.com>
2025-03-21 19:03:28 +0530

    edited some descriptions

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -135,21 +135,16 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Hashir and I am a jack of all trades. My interest in tech started back 
+              in my childhood when I decided to tinker and explore my uncle's workstation which 
+              will manifest to tech craze years later &amp; CSS!
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              <a href="https://www.ltimindtree.com/">a huge corporation</a> 
+              My main focus these days is building and learning innovative solutions, inclusive products and digital
+              experiences as a hooby and carrer upskilling of course!
             </p>
 
             <p>
